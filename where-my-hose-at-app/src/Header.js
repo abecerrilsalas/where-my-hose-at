c9576161cdb913796390455883c1a24071953368
Alma <abecerrilsalas@gmail.com>
2022-01-26 15:50:05 -0800

    header, input field

--- where-my-hose-at-app/src/Header.js
+++ where-my-hose-at-app/src/Header.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import './Header.css';
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 function Header() {
-    return (
-        <div className='header'> 
-            <img 
-                className='header__icon' src='/logo_notext.png' alt='hose logo'
-            />
-            <div className='header__center'>
-                <input type='text' />
-                <SearchIcon />
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <img className="header__icon" src="/logo_notext.png" alt="hose logo" />
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__right">
+        <p>Rent a driveway</p>
+        <AccountCircleIcon />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- where-my-hose-at-app/src/Home.js
+++ where-my-hose-at-app/src/Home.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div className='home'> 
-            <h1>Home Component</h1> 
-
-        </div>
-    )
+  return <div className="home">{/* <h1>Home Component</h1>  */}</div>;
 }
 
-export default Home
+export default Home;
