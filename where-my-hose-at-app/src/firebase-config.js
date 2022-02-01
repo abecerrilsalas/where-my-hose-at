dd2648e959a876ebe0068b05c2888da82076e84d
Alma <abecerrilsalas@gmail.com>
2022-02-01 15:08:58 -0800

    successful login and register routes to /home;

--- where-my-hose-at-app/src/firebase-config.js
+++ where-my-hose-at-app/src/firebase-config.js
@@ -10,7 +10,8 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyCSYaQconBfRGT9-9OrLdhGCtm0kNPiHYg",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
