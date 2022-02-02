395f295d6e2c0c0fa5a246fd651f74fe4a2cbaac
tt-ht <ttht@protonmail.com>
2022-02-01 19:12:01 -0800

    user authentication (login/register), destroy token session on close or with logout button

--- where-my-hose-at-app/src/App.js
+++ where-my-hose-at-app/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
-import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 import Landing from "./components/Landing";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import SearchPage from "./components/SearchPage";
 import Form from "./components/common/Form";
+import Home from "./components/Home";
 
 import { app } from "./firebase-config";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -18,50 +21,85 @@ import {
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  let navigate = useNavigate();
   const handleAction = (id) => {
     const authentication = getAuth();
+    if (id === 1) {
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
+        })
+        .catch((error) => {
+          if (error.code === "auth/wrong-password") {
+            toast.error("Please check the Password");
+          }
+          if (error.code === "auth/user-not-found") {
+            toast.error("Please check the Email");
+          }
+        });
+    }
     if (id === 2) {
-      createUserWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
-          console.log(response);
-        }
-      );
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home");
+          sessionStorage.setItem(
+            "Auth Token",
+            response._tokenResponse.refreshToken
+          );
+        })
+        .catch((error) => {
+          if (error.code === "auth/email-already-in-use") {
+            toast.error("Email Already in Use");
+          }
+        });
     }
   };
+
+  useEffect(() => {
+    let authToken = sessionStorage.getItem("Auth Token");
+
+    if (authToken) {
+      navigate("/home");
+    }
+  }, []);
   return (
     <div className="app">
-      <BrowserRouter>
-        <Header />
+      <ToastContainer />
+      <Header />
 
-        <Routes>
-          <Route
-            path="/login"
-            element={
-              <Form
-                title="Login"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(1)}
-              />
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <Form
-                title="Register"
-                setEmail={setEmail}
-                setPassword={setPassword}
-                handleAction={() => handleAction(2)}
-              />
-            }
-          />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/" element={<Landing />} />
-        </Routes>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route
+          path="/login"
+          element={
+            <Form
+              title="Login"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(1)}
+            />
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Form
+              title="Register"
+              setEmail={setEmail}
+              setPassword={setPassword}
+              handleAction={() => handleAction(2)}
+            />
+          }
+        />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
 
-        <Footer />
-      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
